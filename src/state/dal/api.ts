036214f2374ac536677b7ccf83b44eae0a8627ef
nastassia <anastasiyamihalenko@gmail.com>
2022-03-24 01:04:35 +0300

    githubpages

--- src/state/dal/api.ts
+++ src/state/dal/api.ts
@@ -18,43 +18,4 @@ export const ticketsAPI = {
                     })
             })
     },
-
 }
-
-// axios.get('https://front-test.beta.aviasales.ru/search')
-//     .then(res => {
-//         const key = res.data.searchId
-//         axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${key}`)
-//             .then(response => {
-//                 dispatch(setTickets(response.data.tickets))
-//                 console.log(response.data.tickets)
-//                 console.log("Все билеты")
-//             })
-//             .catch((error) => {
-//                 console.log("error")
-//             })
-//     })
-
-
-//
-//
-// import axios from "axios";
-//
-// async function fetchData() {
-//     try {
-//         const response = await axios.get(
-//             "https://front-test.beta.aviasales.ru/search"
-//         );
-//         const searchId = await response.data.searchId;
-//
-//         const responseTickets = await axios.get(
-//             `https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`
-//         );
-//         return responseTickets.data.tickets;
-//     } catch (e) {
-//         throw new Error("");
-//     }
-// }
-//
-//
-// export { fetchData };
